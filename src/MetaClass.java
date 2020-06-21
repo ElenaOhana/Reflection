ec59cd213c0642e5399a484def71db2839923989
Petina Elena <petinael708@gmail.com>
2020-06-21 18:22:23 +0300

    creating 6 classes

---
+++ src/MetaClass.java
@@ -0,0 +1,7 @@
+public class MetaClass {
+    private String classModifier;
+    private String className;
+    MetaMethod[] methods;
+    MetaConstructor[] constructors;
+    MetaField[] fields;
+}
---
+++ src/MetaMethod.java
@@ -0,0 +1,2 @@
+public class MetaMethod {
+}
