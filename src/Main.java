f82a96f24bb82b5922f075b1d35d04171f6b732f
Petina Elena <petinael708@gmail.com>
2020-06-21 21:27:13 +0300

    intro complete

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,21 @@ public class Main {
         Test test = new Test();
 
         //MetaClass c;
-        Class c; // or clazz
+        Class c; // name of variable #c or #clazz because of #class is the reserved word.
+        c = test.getClass(); // The first technique
+        c = Test.class; // The second technique
+
+        System.out.println(c.getName()); // consists of the name of packet and self name.
+        System.out.println(c.getSimpleName()); // Just a self name
+        System.out.println(TestInterface.class);
+        /*System.out.println(TestInterface.class.getSimpleName());
+        System.out.println(Test.class);
+        System.out.println(MetaClass.class);*/
+
+        TestInterface testInterface = new TestInterface() {
+        }; // anonymous inner class - doesn't has name => getClass().getName() returns only the name of class where is used.
+        TestInterface testInterface2 = new TestInterface() {
+        };
+        System.out.println(testInterface.getClass().getName()); // Main$2 - after $ presents index number
     }
 }
