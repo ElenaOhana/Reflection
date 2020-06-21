67aa4c8f405a6b9fc389a9ca9ea8a68fd2105919
Petina Elena <petinael708@gmail.com>
2020-06-21 18:49:03 +0300

    Class fields discribe MetaClass

--- src/MetaClass.java
+++ src/MetaClass.java
@@ -1,4 +1,4 @@
-public class MetaClass {
+public class MetaClass { // Как бы мы могли описать какой то класс? Из каких элементов состоит класс? Характеристики класса будут являються полями класса.
     private String classModifier;
     private String className;
     MetaMethod[] methods;
