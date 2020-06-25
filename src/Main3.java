7c1e9d0dbc5051fbb2eb4c5fc85be37a26fd45ae
Petina Elena <petinael708@gmail.com>
2020-06-25 14:54:57 +0300

    REFLECTION6: Анализируем пакет

--- src/Main3.java
+++ src/Main3.java
@@ -2,5 +2,7 @@ public class Main3 {
     public static void main(String[] args) {
         Class<TestAbstract> manager = TestAbstract.class;
 
+        Package aPackage = manager.getPackage(); // NullPointerException because of the aPackage:null. TestAbstract class doesn't have an inner package.
+        System.out.printf("package %s;%n", aPackage.getName());
     }
 }
